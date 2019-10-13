1d577f0b00829a363b8007f3b98996f7b087c28f
prakulkumar <randeepkaur18@gmail.com>
2019-10-13 14:39:31 +0530

    link chnage

--- src/config/api-config.js
+++ src/config/api-config.js
@@ -3,9 +3,12 @@ let gatewayApi;
 gatewayApi = process.env.API_URL;
 console.log(process.env.NODE_ENV);
 
-if (process.env.NODE_ENV === "development")
-  gatewayApi = "http://localhost:5000";
-else if (process.env.NODE_ENV === "production")
-  gatewayApi = "https://stepin-api.herokuapp.com";
+// gatewayApi = "http://localhost:5000";
+gatewayApi = "https://stepin-api.herokuapp.com";
+
+// if (process.env.NODE_ENV === "development")
+//   gatewayApi = "http://localhost:5000";
+// else if (process.env.NODE_ENV === "production")
+//   gatewayApi = "https://stepin-api.herokuapp.com";
 
 export const API_ROOT = gatewayApi;
